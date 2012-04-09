58213fd96cf1d0d817f6f7a6e6b2733ac341b8df
Wirithphol Ek-Ularnpun <flashycud@gmail.com>
2012-04-09 10:43:45 +0700

    Changed to jquery mobile 1.1.0-rc2 Added Shop List and Shop pages

--- ShoppingApp/assets/www/js/main.js
+++ ShoppingApp/assets/www/js/main.js
@@ -1,3 +1,13 @@
-(function(){
-	
-})();
+$(document).bind("mobileinit", function(){
+	var touching = null;
+	$('selector').each(function() {
+	    this.addEventListener("touchstart", function(e) {
+	        e.preventDefault();
+	        touching = window.setTimeout(longTouch, 500, true);
+	    }, false);
+	    this.addEventListener("touchend", function(e) {
+	        e.preventDefault();
+	        window.clearTimeout(touching);
+	    }, false);
+	});
+});
